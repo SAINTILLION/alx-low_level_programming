51b645e796f627388202d0bdfc013a59fa74623c
SAINTILLION <ebolommcsaint@gmail.com>
2022-09-11 15:55:21 -0700

    My first commit on task 2 of c vriables project

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <type.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{ int l = 'a';
+while (l <= 'a')
+{
+putchar(l);
+l += 1;
+}
+putchar('\n');
+return (0);
+}
