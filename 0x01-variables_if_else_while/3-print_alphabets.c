ba235de7dc145433fbfac051974f1d4a6a3d38c5
SAINTILLION <ebolommcsaint@gmail.com>
2022-09-11 16:28:27 -0700

    My first commit on task 3 of c variable project

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - program prints the alphabet in lowercase then in uppercase
+ * You can only the putchar twice
+ * Return: 0
+ */
+
+int main(void)
+{
+int lower, upper;
+lower = 'a';
+upper = 'A';
+while (lower <= 'z')
+{ 
+putchar(lower);
+lower += 1;
+}
+while (upper <= 'A')
+{
+putchar(upper);
+upper += 1;
+}
+putchar('\n');
+return (0);
+}
