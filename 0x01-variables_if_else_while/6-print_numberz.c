e736026c20f5867e7171428d69377e9f4e60c04e
SAINTILLION <ebolommcsaint@gmail.com>
2022-09-11 23:22:43 -0700

    This is my first commit on task 6 of c variable project

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the single digits numbers starting from 0
+ * You are only allowed to use putchar twice
+ * Return: 0
+ */
+
+int main(void)
+{
+int n;
+while (n < 10)
+{
+putchar(n);
+n += 1;
+}
+putchar('\n');
+return (0);
+}
