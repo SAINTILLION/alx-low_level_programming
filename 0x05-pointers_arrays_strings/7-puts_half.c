4fe8b8f11b1870f51e46bc2c6187d78ab23b4758
SAINTILLION <ebolommcsaint@gmail.com>
2022-09-20 13:26:32 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a sting
+ * @str: the string to print half
+ */
+void puts_half(char *str)
+{
+	int i;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+	{
+		i = i / 2;
+	}
+	else
+	{
+		i = (i + 1 / 2);
+	}
+	while (str[i] != '\0')
+	{
+		j = str[i];
+		i++;
+		_putchar(j);
+	}
+	_putchar('\n');
+}
