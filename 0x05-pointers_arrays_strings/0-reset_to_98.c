3e10d52aa77f74f74e7a4c50f89f6db1f393ab79
SAINTILLION <ebolommcsaint@gmail.com>
2022-09-20 10:10:35 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as a parameter
+ * and update the value its points to 98
+ * @n: parameter to take the value
+ *
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains files for the pointers, arrays and strings projects
